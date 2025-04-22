46b3156a22037d6e3d87ab0b072bf8ed64ea790c
Akarsha I <akarsha.i@oracle.com>
2025-04-22 16:08:22 +0530

    Initial OCI compute config

---
+++ main.tf
@@ -0,0 +1,23 @@
+# Get the first availability domain
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.tenancy_ocid
+}
+
+# Create a simple compute instance
+resource "oci_core_instance" "test_instance" {
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  compartment_id      = var.compartment_id
+  shape               = "VM.Standard.E2.1.Micro"  # Always Free eligible shape
+  
+  create_vnic_details {
+    subnet_id        = var.subnet_id
+    assign_public_ip = true
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = var.image_id
+  }
+
+  display_name = "TFTestInstance"
+}
---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "tenancy_ocid" {
+  description = "OCID of your tenancy"
+  type        = string
+}
+
+variable "user_ocid" {
+  description = "OCID of the user calling the API"
+  type        = string
+}
+
+variable "fingerprint" {
+  description = "Fingerprint for the key pair being used"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Path to the private key"
+  type        = string
+}
+
+variable "region" {
+  description = "OCI region"
+  type        = string
+  default     = "us-ashburn-1"
+}
+
+variable "compartment_id" {
+  description = "OCID of the compartment where resources will be created"
+  type        = string
+  default = "ocid1.compartment.oc1..aaaaaaaac7ixtdbw32dnr55p4mjsjdmtbfcy5pa5rasdvuw57eup5w7xbu3q"
+}
+
+variable "subnet_id" {
+  description = "OCID of subnet where instance will be created"
+  type        = string
+}
+
+variable "image_id" {
+  description = "OCID of the image to use for the instance"
+  type        = string
+  default     = "ocid1.image.oc1..aaaaaaaadf3m6vj3n36v3z3h3v3z3h3v3z3h3v3z3h3v3z3h3v3z3h3v3z3h" # Oracle Linux 8 image
+}
