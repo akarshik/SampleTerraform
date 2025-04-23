47c475b8f13d173a79e2b1dd263d8863fc0c0e05
Akarsha I <akarsha.i@oracle.com>
2025-04-23 13:50:16 +0530

    variables private key updated

--- providers.tf
+++ providers.tf
@@ -11,6 +11,6 @@ provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
-  private_key_path = "~/.oci/oci_api_key.pem"
+  private_key_path = var.private_key_path
   region           = var.region
 }
